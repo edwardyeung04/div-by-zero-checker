b117598813bc52a79b011c119bf42819868de328
edwardyeung04 <edwardbuddyboy@gmail.com>
2025-02-01 19:07:00 -0800

    update

--- src/main/java/org/checkerframework/checker/dividebyzero/qual/NonZero.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/qual/NonZero.java
@@ -2,11 +2,8 @@ package org.checkerframework.checker.dividebyzero.qual;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import org.checkerframework.framework.qual.SubtypeOf;
 
-@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-@Retention(RetentionPolicy.RUNTIME)
 @SubtypeOf({Top.class})
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface NonZero { }
